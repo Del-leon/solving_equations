024c4dbd46f95334675e5a46cb26ed92fd207606
Del-leon <berd_egor@mail.ru>
2025-07-13 10:54:08 +0300

    [#1] Начало разработки в main_script.py

--- main_script.py
+++ main_script.py
@@ -1,2 +1,27 @@
-input_str = input("Введите уравнение")
+input_str = input("Введите уравнение: ")
 print(f"Введенное уравнение: {input_str}")
+
+coef_x_left = []
+coef_free_left = []
+
+coef_x_right = []
+coef_free_right = []
+
+cash = ""
+for i in input_str:
+
+    if i == "x":
+        if not cash:
+            coef_x_left.append(1)
+        else:
+            coef_x_left.append(int(cash))
+
+        cash = ""
+
+    elif i in "+-*/" and not cash:
+        cash += i
+
+    elif i in "0123456789":
+        cash += i
+
+print(coef_x_left, coef_free_left)
