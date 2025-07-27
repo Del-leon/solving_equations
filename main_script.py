1ce30310b68f3e10b76a37f4c9c51df96d8ab7f6
BerdinEW <berdin.e@arhitex.com>
2025-07-27 10:56:01 +0300

    Исправление замечаний

--- main_script.py
+++ main_script.py
@@ -4,9 +4,8 @@ input_str = input("Введите уравнение: ").replace(" ", "")
 print(f"Уравнение: {input_str}")
 
 input_str = input_str.split("=")
-uravn_left = input_str[0]
+uravn_left, uravn_right = input_str
 print(uravn_left)
-uravn_right = input_str[1]
 print(uravn_right)
 
 coef_x_left, coef_free_left = uravn_plus_and_minus(uravn_left)
