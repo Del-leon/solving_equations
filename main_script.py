bb1a64db1e4f9c0aa508b72bbf9c773eaff4f4a3
Del-leon <85072398+Del-leon@users.noreply.github.com>
2025-07-27 11:05:13 +0300

    переименование и изменение импорта (#6)
    * переименование и изменение импорта
    
    * Исправление замечаний
    
    * Исправление замечаний
    
    * Откат исправлений
    
    ---------
    
    Co-authored-by: BerdinEW <berdin.e@arhitex.com>

--- main_script.py
+++ main_script.py
@@ -1,17 +1,17 @@
-import Operations
+from operations import uravn_plus_and_minus
 
 input_str = input("Введите уравнение: ").replace(" ", "")
 print(f"Уравнение: {input_str}")
 
 input_str = input_str.split("=")
-uravn_left = input_str[0]
+uravn_left, uravn_right = input_str
 print(uravn_left)
-uravn_right = input_str[1]
 print(uravn_right)
 
-coef_x_left, coef_free_left = Operations.uravn_plus_and_minus(uravn_left)
+coef_x_left, coef_free_left = uravn_plus_and_minus(uravn_left)
 
-coef_x_right, coef_free_right = Operations.uravn_plus_and_minus(uravn_right)
+coef_x_right, coef_free_right = uravn_plus_and_minus(uravn_right)
+
 
 print(f"Сумма коэффициентов при х слева{coef_x_left}")
 print(f"Сумма коэффициентов при х справа{coef_x_right}")
